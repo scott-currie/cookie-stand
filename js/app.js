aff4eb953121ae820c1962abb9bb0ede50950496
Scott Currie <vitriolik@gmail.com>
2018-09-17 15:16:38 -0700

    rewrote object declaration

--- js/app.js
+++ js/app.js
@@ -1,50 +1,158 @@
 'use strict';
-
-var firstAndPike = {
+// var locations = [
+var store1 = {
+  name: '1st and Pike',
   minCust: 23,
   maxCust: 65,
   avgCookiesPerSale: 6.3,
-  cookieSales: getSalesByHour()
+  getRandomInRange: function(min, max) {
+    var randInt = Math.ceil(Math.random() * (max - min));
+    console.log('randInt:', randInt);
+    return randInt;
+  },
+  salesByHour: [],
+  getSalesByHour: function() {
+    console.log('this: ', this.minCust, this.maxCust, this.avgCookiesPerSale);
+    var sales = [];
+    var salesThisHour = 0;
+    for (var i = 0; i < 15; i++) {
+      salesThisHour = this.getRandomInRange(this.minCust, this.maxCust) * this.avgCookiesPerSale;
+      console.log('salesThisHour', salesThisHour);
+      sales.push(Math.round(salesThisHour));
+    }
+    console.log('sales: ', sales);
+    this.cookieSales = sales;
+  },
+  // cookieSales: this.getSalesByHour(this.minCust, this.maxCust, this.avgCookiesPerSale)
 };
 
-var seatac = {
+var store2 = {
+  name: 'SeaTac Airport',
   minCust: 3,
   maxCust: 24,
   avgCookiesPerSale: 1.2,
-  cookieSales: getSalesByHour()
+  getRandomInRange: function(min, max) {
+    var randInt = Math.ceil(Math.random() * (max - min));
+    console.log('randInt:', randInt);
+    return randInt;
+  },
+  getSalesByHour: function() {
+    console.log('this: ', this.minCust, this.maxCust, this.avgCookiesPerSale);
+    var sales = [];
+    var salesThisHour = 0;
+    for (var i = 0; i < 15; i++) {
+      salesThisHour = this.getRandomInRange(this.minCust, this.maxCust) * this.avgCookiesPerSale;
+      console.log('salesThisHour', salesThisHour);
+      sales.push(Math.round(salesThisHour));
+    }
+    console.log('sales: ', sales);
+    this.cookieSales = sales;
+  },
+  cookieSales: this.getSalesByHour(this.minCust, this.maxCust, this.avgCookiesPerSale)
 };
 
-var seattleCenter = {
+var store3 = {
+  name: 'Seattle Center',
   minCust: 11,
   maxCust: 38,
   avgCookiesPerSale: 3.7,
-  cookieSales: getSalesByHour()
+  getRandomInRange: function(min, max) {
+    var randInt = Math.ceil(Math.random() * (max - min));
+    console.log('randInt:', randInt);
+    return randInt;
+  },
+  getSalesByHour: function() {
+    console.log('this: ', this.minCust, this.maxCust, this.avgCookiesPerSale);
+    var sales = [];
+    var salesThisHour = 0;
+    for (var i = 0; i < 15; i++) {
+      salesThisHour = this.getRandomInRange(this.minCust, this.maxCust) * this.avgCookiesPerSale;
+      console.log('salesThisHour', salesThisHour);
+      sales.push(Math.round(salesThisHour));
+    }
+    console.log('sales: ', sales);
+    this.cookieSales = sales;
+  },
+  cookieSales: this.getSalesByHour(this.minCust, this.maxCust, this.avgCookiesPerSale)
 };
 
-var capitolHill = {
+var store4 = {
+  name: 'Capitol Hill',
   minCust: 20,
   maxCust: 38,
   avgCookiesPerSale: 2.3,
-  cookieSales: getSalesByHour()
+  getRandomInRange: function(min, max) {
+    var randInt = Math.ceil(Math.random() * (max - min));
+    console.log('randInt:', randInt);
+    return randInt;
+  },
+  getSalesByHour: function() {
+    console.log('this: ', this.minCust, this.maxCust, this.avgCookiesPerSale);
+    var sales = [];
+    var salesThisHour = 0;
+    for (var i = 0; i < 15; i++) {
+      salesThisHour = this.getRandomInRange(this.minCust, this.maxCust) * this.avgCookiesPerSale;
+      console.log('salesThisHour', salesThisHour);
+      sales.push(Math.round(salesThisHour));
+    }
+    console.log('sales: ', sales);
+    this.cookieSales = sales;
+  },
+  cookieSales: this.getSalesByHour(this.minCust, this.maxCust, this.avgCookiesPerSale)
 };
 
-var alki = {
+var store5 = {
+  name: 'Alki',
   minCust: 23,
   maxCust: 65,
   avgCookiesPerSale: 6.3,
-  cookieSales: getSalesByHour()
+  getRandomInRange: function(min, max) {
+    var randInt = Math.ceil(Math.random() * (max - min));
+    console.log('randInt:', randInt);
+    return randInt;
+  },
+  getSalesByHour: function() {
+    console.log('this: ', this.minCust, this.maxCust, this.avgCookiesPerSale);
+    var sales = [];
+    var salesThisHour = 0;
+    for (var i = 0; i < 15; i++) {
+      salesThisHour = this.getRandomInRange(this.minCust, this.maxCust) * this.avgCookiesPerSale;
+      console.log('salesThisHour', salesThisHour);
+      sales.push(Math.round(salesThisHour));
+    }
+    console.log('sales: ', sales);
+    this.cookieSales = sales;
+  },
+  cookieSales: this.getSalesByHour(this.minCust, this.maxCust, this.avgCookiesPerSale)
 };
+var locations = [store1, store2, store3, store4, store5];
 
-var allLocations = [firstAndPike, seatac, seattleCenter, capitolHill, alki];
+// function getRandomInRange(min, max) {
+//   // console.log(min, max);
+//   // var randInt = Math.ceil(Math.random() * (max + 1 - min));
+//   var randInt = Math.ceil(Math.random() * (max - min));
+//   // console.log('randInt:', randInt);
+//   return randInt;
+// }
 
-function getRandomInRange(min, max) {
-  return Math.ceil(Math.random() * (max + 1 - min));
-}
+// function getSalesByHour(min, max, avgSale) {
+//   // console.log(min, max, avgSale);
+//   var sales = [];
+//   var salesThisHour = 0;
+//   for (var i = 0; i < 15; i++) {
+//     // console.log(min, max, avgSale);
+//     salesThisHour = getRandomInRange(min, max) * avgSale;
+//     // console.log(salesThisHour);
+//     sales.push(salesThisHour);
+//   }
+//   return sales;
+// }
 
-function getSalesByHour(min, max, avgSale) {
-  sales = [];
-  for (var i = 0; i < 15;i++) {
-    sales.push(getSalesByHour(min, max) * avgSale);
-  }
-  return sales;
+// console.log('Is anything happening at all?');
+// store1.getSalesByHour();
+for (var i = 0; i < locations.length; i++) {
+  var loc = locations[i];
+  loc.cookieSales = loc.getSalesByHour();
+  loc.getSalesByHour();
+  console.log(loc);
 }
